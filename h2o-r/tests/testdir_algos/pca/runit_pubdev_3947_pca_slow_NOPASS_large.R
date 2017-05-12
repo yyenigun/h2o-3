2ca985bf0f9d6e51ffadc5bb160c312be79f5d9d
wendycwong <wendycwong@yahoo.com>
2017-05-12 09:19:41 -0700

    PUBDEV-3947: pca slow.  Added runit tests to measure pca run time for all models and R.  Minor change to enable widedataset only when it is necessary. PUBDEV-3947: PCA slow.  Time PCA for all PCA methods except GLRM. PUBDEV-3947: PCA slow.  Use uploadFile instead of importFile in runit test."
    PUBDEV-3947: PCA slow.  Add PCA method.

---
+++ h2o-r/tests/testdir_algos/pca/runit_pubdev_3947_pca_slow_NOPASS_large.R
@@ -0,0 +1,44 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source("../../../scripts/h2o-r-test-setup.R")
+
+# Test PCA on car.arff.txt
+test.pca.slow <- function() {
+
+  data = h2o.uploadFile(locate("bigdata/laptop/jira/re0.wc.arff.txt.zip"),destination_frame = "data",header = T)
+  data = data[,-2887]
+  ptm <- proc.time()
+  mm = h2o.prcomp(data,transform = "STANDARDIZE",k =1504, pca_method="GramSVD")
+  h2otimepassed = proc.time() - ptm
+  print(h2otimepassed)
+  h2o.rm(mm)
+
+  ptm <- proc.time()
+  mm = h2o.prcomp(data,transform = "STANDARDIZE",k =1504, pca_method="Power")
+  h2otimepassed = proc.time() - ptm
+  print(h2otimepassed)
+  h2o.rm(mm)
+
+  ptm <- proc.time()
+  mm = h2o.prcomp(data,transform = "STANDARDIZE",k =1504, pca_method="Randomized")
+  h2otimepassed = proc.time() - ptm
+  print(h2otimepassed)
+  h2o.rm(mm)
+
+  ptm <- proc.time()
+  mm = h2o.prcomp(data,transform = "STANDARDIZE",k =1504, pca_method="GLRM")
+  h2otimepassed = proc.time() - ptm
+  print(h2otimepassed)
+  h2o.rm(mm)
+  h2o.rm(data)
+
+  data = read.csv(locate("bigdata/laptop/jira/re0.wc.arff.txt.zip"))
+  data = data[,-2887]
+  ptm <- proc.time()
+  fitR <- prcomp(data, center = T, scale. = T)
+  timepassed = proc.time() - ptm
+  print(timepassed)
+
+
+}
+
+doTest("PCA Test: rec0.wc.arff.txt", test.pca.slow)
