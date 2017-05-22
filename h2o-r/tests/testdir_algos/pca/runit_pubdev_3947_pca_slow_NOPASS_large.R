1c9a7f62f83c47520262e5ea69fa6bf61371406b
wendycwong <wendycwong@yahoo.com>
2017-05-22 09:30:16 -0700

    PUBDEV-3947: PCA slow.  Fixed bug in R unit test.

--- h2o-r/tests/testdir_algos/pca/runit_pubdev_3947_pca_slow_NOPASS_large.R
+++ h2o-r/tests/testdir_algos/pca/runit_pubdev_3947_pca_slow_NOPASS_large.R
@@ -6,6 +6,22 @@ test.pca.slow <- function() {
 
   data = h2o.uploadFile(locate("bigdata/laptop/jira/re0.wc.arff.txt.zip"),destination_frame = "data",header = T)
   data = data[,-2887]
+
+  ptm <- proc.time()
+  mm = h2o.prcomp(data,transform = "STANDARDIZE",k =1504, pca_method="GLRM", , use_all_factor_levels=TRUE)
+  h2otimepassed = proc.time() - ptm
+  print(h2otimepassed)
+  h2o.rm(mm)
+  h2o.rm(data)
+
+
+  ptm <- proc.time()
+  mm = h2o.prcomp(data,transform = "STANDARDIZE",k =1504, max_iterations=10, pca_method="Randomized")
+  h2otimepassed = proc.time() - ptm
+  print(h2otimepassed)
+  h2o.rm(mm)
+
+
   ptm <- proc.time()
   mm = h2o.prcomp(data,transform = "STANDARDIZE",k =1504, pca_method="GramSVD")
   h2otimepassed = proc.time() - ptm
@@ -17,20 +33,7 @@ test.pca.slow <- function() {
   h2otimepassed = proc.time() - ptm
   print(h2otimepassed)
   h2o.rm(mm)
-
-  ptm <- proc.time()
-  mm = h2o.prcomp(data,transform = "STANDARDIZE",k =1504, pca_method="Randomized")
-  h2otimepassed = proc.time() - ptm
-  print(h2otimepassed)
-  h2o.rm(mm)
-
-  ptm <- proc.time()
-  mm = h2o.prcomp(data,transform = "STANDARDIZE",k =1504, pca_method="GLRM")
-  h2otimepassed = proc.time() - ptm
-  print(h2otimepassed)
-  h2o.rm(mm)
-  h2o.rm(data)
-
+  
   data = read.csv(locate("bigdata/laptop/jira/re0.wc.arff.txt.zip"))
   data = data[,-2887]
   ptm <- proc.time()
